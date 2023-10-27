ce2e218d64aa0c20d75bde4264c3c715090836e9
Murilo Corso <murilofcorso@gmail.com>
2023-10-27 18:04:29 -0300

    app(conexao db): criacao e teste do conector

--- gerenciamento_livros/src/main/java/com/booklinx/gerenciamento_livros/ConnectionFactory.java
+++ gerenciamento_livros/src/main/java/com/booklinx/gerenciamento_livros/ConnectionFactory.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.booklinx.gerenciamento_livros;
 
-/**
- *
- * @author muril
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
 public class ConnectionFactory {
+    private final String host = "localhost";
+    private final String port = "3306";
+    private final String user = "root";
+    private final String password = "1234";
+    private final String db = "db_booklinx";
     
+    public Connection conectar() throws Exception {
+        //String de conexão
+        //String s = "jdbc:mysql://" + host + ":" + port + "/" + db;
+        String s = String.format(
+            "jdbc:mysql://%s:%s/%s",
+            host, port, db
+        );
+        
+        Connection c = DriverManager.getConnection(
+                s, 
+                user, 
+                password
+        );
+        return c;
+    }    
 }
--- gerenciamento_livros/src/main/java/com/booklinx/gerenciamento_livros/Usuario.java
+++ gerenciamento_livros/src/main/java/com/booklinx/gerenciamento_livros/Usuario.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.booklinx.gerenciamento_livros;
 
-/**
- *
- * @author muril
- */
 public class Usuario {
     private int id;
     private String nome;
