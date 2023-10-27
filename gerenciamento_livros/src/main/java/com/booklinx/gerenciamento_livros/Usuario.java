c54372724043c074cbec2152646eb3dae1ba4873
Murilo Corso <murilofcorso@gmail.com>
2023-10-27 17:36:05 -0300

    app(Usuario): criacao da classe Usuario

---
+++ gerenciamento_livros/src/main/java/com/booklinx/gerenciamento_livros/Usuario.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.booklinx.gerenciamento_livros;
+
+/**
+ *
+ * @author muril
+ */
+public class Usuario {
+    private int id;
+    private String nome;
+    private int idade;
+    private String sexo;
+    
+    public Usuario(String nome, int idade, String sexo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public int getIdade() {
+        return idade;
+    }
+    
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    public String getSexo() {
+        return sexo;
+    }
+    
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+}
