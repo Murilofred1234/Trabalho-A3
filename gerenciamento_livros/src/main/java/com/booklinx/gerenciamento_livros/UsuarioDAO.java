cea1cd91294172bd6a4a6d4c6363a30b07b689b9
Murilo Corso <murilofcorso@gmail.com>
2023-11-06 16:49:17 -0300

    exclusao da classe CadastroUsuario

--- gerenciamento_livros/src/main/java/com/booklinx/gerenciamento_livros/UsuarioDAO.java
+++ gerenciamento_livros/src/main/java/com/booklinx/gerenciamento_livros/UsuarioDAO.java
@@ -9,20 +9,15 @@ public class UsuarioDAO {
         
         // 2. abrir conexao com o MySQL
         var fabricaDeConexoes = new ConnectionFactory();
-        var conexao = fabricaDeConexoes.conectar();
-        
-        // 3. preparar o comando
-        PreparedStatement ps = conexao.prepareStatement(sql);
-        
-        // 4. substituir os eventuais placeholders
-        ps.setString(1, u.getNome());
-        ps.setInt(2, u.getIdade());
-        ps.setString(3, u.getSexo());
-        
-        // 5. executar o comando preparado
-        ps.execute();
-        
-        // 6. fechar a conexao
-        conexao.close();
+        try (java.sql.Connection conexao = fabricaDeConexoes.conectar()) {
+            // 3. preparar o comando
+            PreparedStatement ps = conexao.prepareStatement(sql);
+            // 4. substituir os eventuais placeholders
+            ps.setString(1, u.getNome());
+            ps.setInt(2, u.getIdade());
+            ps.setString(3, u.getSexo());
+            // 5. executar o comando preparado
+            ps.execute();
+        }
     }
 }
